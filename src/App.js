fa89a041bd8229912e2e97d9fb8a6702b6f08a33
refriansyari <65706381+refriansyari@users.noreply.github.com>
2020-12-05 14:31:30 +0700

    HTML CSS migration to React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import React from "react";
 import "./styles.css";
 
+// Menjalankan fungsi yang mereturn skrip JSX
+// Deklarasi sebuah variabel bernama App
+// export default berfungsi mengeksport file ke directory lain untuk di import
 export default function App() {
+  // JSX skrip not JS
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    // class to className in JSX
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-btn main-black-color">Add</button>
+          <h1 className="header-title">ToDo Lists</h1>
+          <button className="header-btn main-red-color">Clear</button>
+        </section>
+
+        <section className="add">
+          <form className="add-form">
+            <input type="text" className="add-input" />
+            <button className="add-button main-black-color">Add</button>
+          </form>
+        </section>
+        <section className="todos">
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//Memanggil sebuah variabel App dari directory ./App
 import App from "./App";
 
 const rootElement = document.getElementById("root");
+//method untuk me-render element App ke dalam DOM
 ReactDOM.render(
   <React.StrictMode>
     <App />
